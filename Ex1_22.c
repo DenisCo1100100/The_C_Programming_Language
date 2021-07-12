8cb9b3e8b3dff41df5c4f31012eac353869e2da3
DenisCo1100100 <kodek_den2001@mail.ru>
2021-07-12 13:30:43 +0300

    Moved algorithms and added Ex1_22

--- Ex1_22.c
+++ Ex1_22.c
@@ -1,36 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define MAXLINE 1000
 
-int getline(char line[], int lim,int n);
-void print_array();
-
-int main()
-{
-	char line[MAXLINE];
-	int length;
-	int n = getchar();
-
-	length = getline(line, MAXLINE, n);
-	reverse(line, length);
-}
-
 int getline(char line[], int lim, int n)
 {
-	int c, i;
+	int c = getchar();
+	int i;
 	int lim_count = 0;
-	
-	for (i = 0; i < lim - 1 && (c = getchar()) != EOF; ++i)
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF;i++)
 	{
 		lim_count++;
-		
-		if((c == ' ' || c == '\t') && i >= n)
+		//
+		if ((c == ' ' || c == '\t') && lim_count >= n)
 		{
 			line[i] = '\n';
 			lim_count = 0;
-			++i;
+		}
+		else
+		{
+			line[i] = c;
 		}
 		
-		line[i] = c;
 	}
 
 	if (c == '\n')
@@ -43,7 +34,21 @@ int getline(char line[], int lim, int n)
 	return i;
 }
 
-void print_array(char arr[])
+void print_array(char arr[], int length)
 {
-	
+	for (int i = 0; i < length; ++i)
+	{
+		putchar(arr[i]);
+	}
+}
+
+int main()
+{
+	char line[MAXLINE];
+	int length;
+	int n;
+	scanf("%d", &n);
+
+	length = getline(line, MAXLINE, n);
+	print_array(line, length);
 }
